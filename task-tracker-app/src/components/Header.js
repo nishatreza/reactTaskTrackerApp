46db9975a7996eec1eba38cf9afd5e01fb78f04a
M. Mobasher Reza <30533488+nishatreza@users.noreply.github.com>
2022-01-27 01:58:40 +0600

    3rd
    3rd

--- task-tracker-app/src/components/Header.js
+++ task-tracker-app/src/components/Header.js
@@ -5,11 +5,18 @@ import Button from './Button';
 
 
 const Header = ({ title }) => {
+
+    const onClick = () => {
+        console.log('clicked!')
+
+    }
+
+
     return (
 
         <header className='header'>
             <h1>{title} </h1>
-            <Button />
+            <Button color='green' text='Add' onClick={onClick} />
         </header>
 
     )
