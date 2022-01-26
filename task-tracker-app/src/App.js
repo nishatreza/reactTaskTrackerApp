8cd2ef28c5483a66d1940c8e183eb85af6971f54
M. Mobasher Reza <30533488+nishatreza@users.noreply.github.com>
2022-01-27 01:18:12 +0600

    2nd
    2nd

--- task-tracker-app/src/App.js
+++ task-tracker-app/src/App.js
@@ -1,9 +1,11 @@
+import Header from "./components/Header";
 
-
-function App() {
+const App = () => {
   return (
     <div className="container">
-      <h1>Hello from React!</h1>
+      <Header />
+
+
     </div>
   );
 }
