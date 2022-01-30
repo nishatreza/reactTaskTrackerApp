9c5aad0e852728438a310a0010d00e06c65a4240
M. Mobasher Reza <30533488+nishatreza@users.noreply.github.com>
2022-01-30 15:14:46 +0600

    9th
    9th

--- task-tracker-app/src/App.js
+++ task-tracker-app/src/App.js
@@ -1,6 +1,6 @@
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import AddTask from "./components/AddTask";
 
 
@@ -8,30 +8,30 @@ const App = () => {
 
   const [showAddTask, setShowAddTask] = useState(false)
 
-  const [tasks, setTasks] = useState([
+  const [tasks, setTasks] = useState([])
 
-    {
-      id: 1,
-      text: 'Doctors Appointment',
-      day: 'Feb 5th at 2:30 PM',
-      reminder: true
-    },
 
-    {
-      id: 2,
-      text: 'Meeting at School',
-      day: 'Feb 6th at 2:30 PM',
-      reminder: true
-    },
+  useEffect(() => {
 
-    {
-      id: 3,
-      text: 'Food Shopping',
-      day: 'Dec 7th at 2:30 PM',
-      reminder: false
-    },
+    const getTasks = async () => {
 
-  ])
+      const tasksFromServer = await fetchTasks()
+      setTasks(tasksFromServer)
+    }
+
+    getTasks()
+
+  }, [])
+
+
+  // fetch tasks
+  const fetchTasks = async () => {
+    const res = await fetch("http://localhost:5000/tasks")
+    const data = await res.json()
+
+    return data
+
+  }
 
   // add task
 
